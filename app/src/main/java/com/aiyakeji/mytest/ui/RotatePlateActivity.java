68d932fa22c537e4c883d4db3913eb1f4c58ffbf
caiwenqing <qianyucha@foxmail.com>
2023-06-06 14:37:57 +0800

    优化刻度盘角度计算

--- app/src/main/java/com/aiyakeji/mytest/ui/RotatePlateActivity.java
+++ app/src/main/java/com/aiyakeji/mytest/ui/RotatePlateActivity.java
@@ -1,16 +1,15 @@
 package com.aiyakeji.mytest.ui;
 
 import android.os.Bundle;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.aiyakeji.mytest.R;
 import com.aiyakeji.mytest.widgets.ScalePanelView;
-
-import static com.aiyakeji.mytest.R.id.seven_btn_right;
 
 /**
  * 自定义圆盘
@@ -18,9 +17,7 @@ import static com.aiyakeji.mytest.R.id.seven_btn_right;
 
 public class RotatePlateActivity extends AppCompatActivity implements View.OnClickListener {
     private ScalePanelView ccv;
-    private Button btn_left;
     private TextView tv;
-    private Button btn_right;
     private Button btn_forbit;
 
 
@@ -33,46 +30,29 @@ public class RotatePlateActivity extends AppCompatActivity implements View.OnCli
 
     private void initView() {
         ccv = (ScalePanelView) findViewById(R.id.seven_ccv);
-        btn_left = (Button) findViewById(R.id.seven_btn_left);
         tv = (TextView) findViewById(R.id.seven_tv);
-        btn_right = (Button) findViewById(seven_btn_right);
         btn_forbit = (Button) findViewById(R.id.seven_btn_forbit);
 
 
         ccv.setOnRotate(new ScalePanelView.onRotateListener() {
             @Override
-            public void onRotateLeft(int num) {
-                tv.setText("向左旋转" + num + "格");
-            }
-
-            @Override
-            public void onRotateRight(int num) {
-                tv.setText("向右旋转" + num + "格");
+            public void onRotate(float angle) {
+                tv.setText("角度：" + angle);
             }
         });
-        btn_left.setOnClickListener(this);
-        btn_right.setOnClickListener(this);
         btn_forbit.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.seven_btn_left:
-                ccv.rotateLeft();
-                break;
-            case R.id.seven_btn_right:
-                ccv.rotateRight();
-                break;
-            case R.id.seven_btn_forbit:
-                if (ccv.getForbitRotate()) {
-                    ccv.setForbitRotate(false);
-                    btn_forbit.setText("禁止");
-                } else {
-                    ccv.setForbitRotate(true);
-                    btn_forbit.setText("开启");
-                }
-                break;
+        if (v.getId() == R.id.seven_btn_forbit) {
+            if (ccv.getForbidRotate()) {
+                ccv.setForbidRotate(false);
+                btn_forbit.setText("禁止");
+            } else {
+                ccv.setForbidRotate(true);
+                btn_forbit.setText("开启");
+            }
         }
     }
 }
--- app/src/main/java/com/aiyakeji/mytest/widgets/ScalePanelView.java
+++ app/src/main/java/com/aiyakeji/mytest/widgets/ScalePanelView.java
@@ -1,5 +1,6 @@
 package com.aiyakeji.mytest.widgets;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
@@ -9,18 +10,16 @@ import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Shader;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 
 import androidx.annotation.Nullable;
 
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 import com.aiyakeji.mytest.R;
+import com.aiyakeji.mytest.utils.BitmapUtils;
 
 /**
  * 自定义刻度盘
@@ -34,23 +33,20 @@ public class ScalePanelView extends View {
     private float mCircleBitmapRadio;//背景图片圆半径
     private Bitmap bgBitmap;
 
-    private float mLongClockLine = 40;//长表盘指针长度
-    private int mClockNum = 120;//最大指针数量
-    private float mPreAngle;//每个指针间偏移角度
+    private final float mLongClockLine = 40;//长表盘指针长度
+    private final int mClockNum = 120;//最大指针数量
+    private final float mPreAngle;//每个指针间偏移角度
 
-    private Paint mClockPaint = new Paint();//指针画笔
-    private Paint mBgPaint = new Paint();//背景图片画笔
+    private final Paint mClockPaint = new Paint();//指针画笔
+    private final Paint mBgPaint = new Paint();//背景图片画笔
 
     private BitmapShader bitmapShader;
-    private Matrix mMatrix;
+    private final Matrix mMatrix = new Matrix();
     private int mPaintColor = Color.WHITE;
     private boolean mForbidRotate = false;//是否禁止旋转
-
-    private float touchRotate = 0;
-    private float downX;
-    private float downY;
-    private float movingX;
-    private float movingY;
+    private float mStartAngle;
+    private float mRotationAngle;
+    private float mRawRotationAngle;
 
     public ScalePanelView(Context context) {
         this(context, null);
@@ -65,7 +61,6 @@ public class ScalePanelView extends View {
         setClickable(true);
         initAttrs(attrs);
 
-        mMatrix = new Matrix();
         mBgPaint.setAntiAlias(true);
 
         mClockPaint.setAntiAlias(true);
@@ -78,7 +73,7 @@ public class ScalePanelView extends View {
         TypedArray attribute = getContext().obtainStyledAttributes(attrs, R.styleable.ControlableCircleView);
         Drawable bgDrawable = attribute.getDrawable(R.styleable.ControlableCircleView_circle_bg);
         attribute.recycle();
-        bgBitmap = getBitmapFromDrawable(bgDrawable);
+        bgBitmap = BitmapUtils.getBitmapFromDrawable(bgDrawable);
         if (bgBitmap != null) {
             bitmapShader = new BitmapShader(bgBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
         }
@@ -90,7 +85,6 @@ public class ScalePanelView extends View {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         mWidth = getMeasuredWidth();
         mHeight = getMeasuredHeight();
-        Log.d(TAG, "onMeasure: width:" + mWidth + ",height:" + mHeight);
         //长短指针所在的圆半径
         mClockRadio = Math.min(mWidth, mHeight) / 2 - mLongClockLine;
         //背景图片圆半径
@@ -101,7 +95,7 @@ public class ScalePanelView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.rotate(touchRotate, mWidth / 2, mHeight / 2);
+        canvas.rotate(mRotationAngle, mWidth / 2, mHeight / 2);
         drawBg(canvas);
         //画长短指针
         for (int i = 0; i < mClockNum; i++) {
@@ -134,106 +128,50 @@ public class ScalePanelView extends View {
     }
 
 
-    private Bitmap getBitmapFromDrawable(Drawable drawable) {
-        if (drawable == null) {
-            return null;
-        }
+    private float getAngleForPoint(float p1X, float p1Y) {
+        double tx = p1X - mWidth / 2, ty = p1Y - mHeight / 2;
+        double length = Math.sqrt(tx * tx + ty * ty);
+        double r = Math.acos(ty / length);
 
-        if (drawable instanceof BitmapDrawable) {
-            return ((BitmapDrawable) drawable).getBitmap();
-        }
+        float angle = (float) Math.toDegrees(r);
 
-        try {
-            Bitmap bitmap;
+        if (p1X > mWidth / 2)
+            angle = 360f - angle;
 
-            if (drawable instanceof ColorDrawable) {
-                bitmap = Bitmap.createBitmap(2, 2, Bitmap.Config.ARGB_8888);
-            } else {
-                bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-            }
+        angle = angle + 90f;
 
-            Canvas canvas = new Canvas(bitmap);
-            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-            drawable.draw(canvas);
-            return bitmap;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+        if (angle > 360f)
+            angle = angle - 360f;
+
+        return angle;
     }
 
 
-    /**
-     * 计算三个点的夹角
-     *
-     * @param p1X
-     * @param p1Y
-     * @param centerX
-     * @param centerY
-     * @param p2X
-     * @param p2Y
-     * @return
-     */
-    private float calcAngle(float p1X, float p1Y, float centerX, float centerY, float p2X, float p2Y) {
-        float dis1 = calcDisBetweenPoint(p1X, p1Y, centerX, centerY);
-        float sin1 = (centerX - p1X) / dis1;
-        float dis2 = calcDisBetweenPoint(centerX, centerY, p2X, p2Y);
-        float sin2 = (p2X - centerX) / dis2;
-        return (float) ((Math.asin(sin1) + Math.asin(sin2)) / Math.PI * 180);
-    }
+    private float getNormalizedAngle(float angle) {
+        while (angle < 0.f)
+            angle += 360.f;
 
-    /**
-     * 计算两个点中间的距离
-     *
-     * @param x1
-     * @param y1
-     * @param x2
-     * @param y2
-     * @return
-     */
-    private float calcDisBetweenPoint(float x1, float y1, float x2, float y2) {
-        float disX = Math.abs(x1 - x2);
-        float disY = Math.abs(y1 - y2);
-        return (float) Math.sqrt(disX * disX + disY * disY);
+        return angle % 360.f;
     }
 
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (!mForbidRotate)
             switch (event.getAction()) {
                 case MotionEvent.ACTION_DOWN:
-                    downX = event.getX();
-                    downY = event.getY();
+                    mStartAngle = getAngleForPoint(event.getX(), event.getY()) - mRawRotationAngle;
                     break;
                 case MotionEvent.ACTION_MOVE:
-                    movingX = event.getX();
-                    movingY = event.getY();
-                    //计算下落点、圆心、移动点三点间的夹角
-                    float angle = calcAngle(downX, downY, mWidth / 2, mHeight / 2, movingX, movingY);
-                    if (movingY > mHeight / 2) {
-                        angle = -angle;
+                    float angle = getAngleForPoint(event.getX(), event.getY());
+                    mRawRotationAngle = angle - mStartAngle;
+                    mRotationAngle = getNormalizedAngle(mRawRotationAngle);
+//                    Log.d(TAG, "onTouchEvent: angle:" + angle + ",mRawRotationAngle:" + mRawRotationAngle + ",mRotationAngle:" + mRotationAngle);
+                    if (mOnRotateListener != null){
+                        mOnRotateListener.onRotate(mRotationAngle);
                     }
-//                    Log.d(TAG, "onTouchEvent: angle:" + angle + ",downX:" + downX + ",downY:" + downY + ",movingX:" + movingX + ",movingY:" + movingY);
-                    if (Math.abs(angle) >= mPreAngle) {
-                        if (angle > 0) {
-                            angle = mPreAngle;
-                            if (mOnRotateListener != null) {
-                                mOnRotateListener.onRotateRight(1);
-                            }
-                        } else {
-                            angle = -mPreAngle;
-                            if (mOnRotateListener != null) {
-                                mOnRotateListener.onRotateLeft(1);
-                            }
-                        }
-
-                        touchRotate += angle;
-
-                        downX = movingX;
-                        downY = movingY;
-                        invalidate();
-                    }
+                    invalidate();
                     break;
                 case MotionEvent.ACTION_UP:
                     break;
@@ -244,37 +182,12 @@ public class ScalePanelView extends View {
 
 
     /**
-     * 向左旋转一格
-     */
-    public void rotateLeft() {
-        if (!mForbidRotate) {
-            touchRotate -= mPreAngle;
-            if (null != mOnRotateListener)
-                mOnRotateListener.onRotateLeft(1);
-            invalidate();
-        }
-    }
-
-    /**
-     * 向右旋转一格
-     */
-    public void rotateRight() {
-        if (!mForbidRotate) {
-            touchRotate += mPreAngle;
-            if (null != mOnRotateListener)
-                mOnRotateListener.onRotateRight(1);
-            invalidate();
-        }
-    }
-
-
-    /**
      * 设置是否禁止旋转
      *
-     * @param forbitRotate
+     * @param forbidRotate
      */
-    public void setForbitRotate(boolean forbitRotate) {
-        mForbidRotate = forbitRotate;
+    public void setForbidRotate(boolean forbidRotate) {
+        mForbidRotate = forbidRotate;
     }
 
 
@@ -283,7 +196,7 @@ public class ScalePanelView extends View {
      *
      * @return
      */
-    public boolean getForbitRotate() {
+    public boolean getForbidRotate() {
         return mForbidRotate;
     }
 
@@ -292,9 +205,7 @@ public class ScalePanelView extends View {
     private onRotateListener mOnRotateListener;
 
     public interface onRotateListener {
-        void onRotateLeft(int num);
-
-        void onRotateRight(int num);
+        void onRotate(float angle);
     }
 
     public void setOnRotate(onRotateListener onRotateListener) {
